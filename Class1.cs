7d680912c8b819b8f59600494b801e7dfb87508f
Kevin <kevin.sanchezpeep@gmail.com>
2022-12-11 21:27:12 -0400

    added tests

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace DotNet.Docker
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(1, 2);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ Class2.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace DotNet.Docker
+{
+    [TestFixture]
+    public class Class2
+    {
+        [Test]
+        public void Test4()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [Test]
+        public void Test5()
+        {
+            Assert.AreEqual(1, 2);
+        }
+
+        [Test]
+        public void Test6()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
